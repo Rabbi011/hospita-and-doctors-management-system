ea72bc970531d9add71234b5cb846606e9b9e876
RAKIBUL HASAN <rakibul15-13011@diu.edu.bd>
2023-04-29 13:14:59 +0600

    add primary button

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import char from '../../../assets/images/chair.png';
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
+import bg from '../../../assets/images/bg.png'
+
 const Banner = () => {
     return (
-        <div>
-            
+        <div className="hero" style={{background: `url(${bg})`}}>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={char} className=" rounded-lg shadow-2xl lg:w-1/2" alt='' />
+                <div>
+                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <PrimaryButton>Get Started</PrimaryButton>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import InfoCards from '../InfoCards/InfoCards';
+import Services from '../Services/Services';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
+
 
 const Home = () => {
     return (
         <div className='mx-5'>
-            <h2>This is home</h2>
+            <Banner></Banner>
+            <InfoCards></InfoCards>
+            <Services></Services>
+            <MakeAppointment></MakeAppointment>
+            
         </div>
     );
 };
