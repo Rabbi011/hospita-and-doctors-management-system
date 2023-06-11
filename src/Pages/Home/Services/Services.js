c48f7408a6122ce3c5a44774380f2ef21d0e603e
RAKIBUL HASAN <rakibul15-13011@diu.edu.bd>
2023-06-12 00:22:18 +0600

    add some services

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import fluoride from '../../../assets/images/fluoride.png'
 import cavity from '../../../assets/images/cavity.png'
-import whitening from '../../../assets/images/whitening.png'
+import whitening from '../../../assets/images/whitening.png';
+import FreeCheckup from '../../../assets/images/FreeCheckup.jpg';
+import Medicines from '../../../assets/images/Medicines.jpg';
+import Ambulance from '../../../assets/images/Ambulance.jpg';
 import Service from './Service';
 
 const Services = () => {
@@ -11,19 +14,37 @@ const Services = () => {
             id: 1,
             name: 'Fluoride Treatment',
             description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
-            img: fluoride
+            img: cavity
         },
         {
             id: 2,
-            name: 'Cavity Filling',
+            name: '24/7 Ambulance',
             description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
-            img: cavity
+            img: Ambulance
         },
         {
             id: 3,
             name: 'Teeth Whitening',
             description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
             img: whitening
+        },
+        {
+            id: 4,
+            name: 'Free Checkup',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: FreeCheckup
+        },
+        {
+            id: 5,
+            name: 'Medicines',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: Medicines
+        },
+        {
+            id: 6,
+            name: 'Cavity Filling',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: fluoride 
         },
     ]
 
